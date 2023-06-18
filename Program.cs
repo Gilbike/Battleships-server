b1fca70315bc80a7c6a7c9480df08f7d6762ca6c
Gilbike <marton.dikacz@gmail.com>
2023-06-18 09:36:28 +0200

    client connection

--- Program.cs
+++ Program.cs
@@ -4,16 +4,21 @@ using LiteNetLib.Utils;
 EventBasedNetListener listener = new EventBasedNetListener();
 NetManager server = new NetManager(listener);
 server.Start(9050);
+System.Console.WriteLine("[Server] Server started on :9050");
 
 listener.ConnectionRequestEvent += request => {
-  if (server.ConnectedPeersCount < 10)
+  System.Console.WriteLine("[Server] Incoming connection request");
+  if (server.ConnectedPeersCount < 10) {
+    System.Console.WriteLine("[Server] Accepted connection request");
     request.AcceptIfKey("SomeConnectionKey");
-  else
+  } else {
+    System.Console.WriteLine("[Server] Rejected connection request");
     request.Reject();
+  }
 };
 
 listener.PeerConnectedEvent += peer => {
-  Console.WriteLine("We got connection: {0}", peer.EndPoint);
+  Console.WriteLine("[Server] Client connection from: {0}", peer.EndPoint.Address);
   NetDataWriter writer = new NetDataWriter();
   writer.Put("Hello client!");
   peer.Send(writer, DeliveryMethod.ReliableOrdered);
